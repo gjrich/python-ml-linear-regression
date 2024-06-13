84a0f4911aba13e93f2460290aec2cc7c28df46d
Denise Case <denisecase@users.noreply.github.com>
2024-06-13 11:52:25 -0500

    Update app.py

--- app_lr.py
+++ app_lr.py
@@ -1,11 +1,19 @@
 ################################
+# app.py
+################################
+
+################################
 # Import Packages First
 ################################
 
 # Python Standard Library Packages
 import random
 import statistics
-from typing import List, Tuple
+from typing import List, Tuple, Callable
+
+# External Packages (must be installed)
+# Use requirements.txt
+# List one package per line
 
 # Plotly for interactive charts
 import plotly.express as px
@@ -13,13 +21,13 @@ import plotly.graph_objects as go
 from plotly.graph_objs._figure import Figure
 
 # PyShiny packages
-from shiny import render
+from shiny import render, reactive
 from shiny.express import ui, input
 from shinywidgets import render_plotly
 
-# Import from our local utility 
-# list everything we use in this file
-from util_lr import xlist, ylist, xlist2, ylist2, function_list
+# Local module import - use util_lr
+# list all the top-level variables and functions we need in app.py
+from util_lr import xlist, ylist, xlist2, ylist2, function_list, function_mapping, description_mapping, get_slope_and_intercept
 
 ################################
 # Define Global Variables
@@ -27,7 +35,6 @@ from util_lr import xlist, ylist, xlist2, ylist2, function_list
 
 # Create dictionary for dropdown options
 function_options_dict = {
-    "none": "None", 
     "stat": "Std Lib Statistics",
     "numpy": "NumPy",
     "scipy": "SciPy",
@@ -37,170 +44,129 @@ function_options_dict = {
     "ransac": "Scikit-Learn RANSAC",
 }
 
-# Create a function mapping with placeholder functions
-def basic_linear_regression(xlist: List[float], ylist: List[float]) -> Tuple[float, float]:
-    return statistics.linear_regression(xlist, ylist)
-
-# Create a function mapping
-function_mapping = {name: basic_linear_regression for name, _, _ in function_list}
-
-
 ################################
 # Define the PyShiny Express UI
 ################################
 
 # Use the PyShiny ui.page_opts() method to set the title 
-ui.page_opts(title="Linear Regression Plot")
+ui.page_opts(title="Why Do We Square Things in Linear Regression?")
 
 with ui.sidebar(bg="#EEF2F8"):  
-    "Sidebar" 
+    "User Selections" 
 
     ui.input_select(
-        "f1",
-        "Choose a function:",
+        "selected_first_method",
+        "Method 1",
         function_options_dict,
         selected="stat"
     )
 
     ui.input_select(
-        "f2",
-        "Choose a function:",
+        "selected_second_method",
+        "Method 2:",
         function_options_dict,
          selected="huber"
     )
 
-    
+    ui.input_select(
+        "selected_dataset",
+        "Choose a dataset:",
+        {"1":"Dataset 1",
+        "2":"Dataset 2"},
+         selected="1"
+    )
+
+
+################################
+# Reactive calcs 
+# Update automatically when inputs change
+################################
+
+# Reactive calculations for selected functions
+@reactive.calc
+def selected_function_1():
+    return function_mapping[input.selected_first_method()]
+
+@reactive.calc
+def selected_function_2():
+    return function_mapping[input.selected_second_method()]
+
+@reactive.calc
+def getXY():
+    if input.selected_dataset() == "1":
+        return xlist, ylist
+    else:
+        return xlist2, ylist2
 
 ################################
 # Define & Render Output
 ################################
 
 with ui.layout_columns(col_widths=(6,6)):
+    
     with ui.card():
         
         @render.text
+        def method1():
+            return f"{function_options_dict[input.selected_first_method()]}"
+
+        @render.text
         def descrip1():
-            return f"{function_options_dict[input.f1()]}"
-        
+            return f"{description_mapping[input.selected_first_method()]}"
+ 
         @render_plotly
         def scatter_plot1() -> Figure:
             ''' Define a function that returns a Plotly Figure'''
-        
-            # Fit linear regression model given our x and y lists
-            slope, intercept = statistics.linear_regression(xlist, ylist)
-        
+
+            method = input.selected_first_method()
+            xvalues, yvalues = getXY()
+            slope, intercept = get_slope_and_intercept(xvalues, yvalues, method)
+            
             # Create a new plotly scatter chart from our data
             fig = px.scatter(
-                x=xlist, 
-                y=ylist, 
+                x=xvalues, 
+                y=yvalues, 
                 labels={'x': 'X', 'y': 'Y'}, 
-                title=f"Dataset 1 (Slope: {slope:.3f}, Intercept: {intercept:.3f})")
-        
+                title=f"Slope: {slope:.3f}, Intercept: {intercept:.3f}")
+       
             # Calculate the regression line using list comprehension
             # Return mx+b for each x in the x list
-            regression_line = [slope * x + intercept for x in xlist]
+            regression_line = [slope * x + intercept for x in xvalues]
         
             # Create a plotly line using px.line() constructor method.
             # Use the figure's add_trace() method
             # Pass in the new line we created to add it to the chart
-            fig.add_trace(px.line(x=xlist, y=regression_line).data[0])
+            fig.add_trace(px.line(x=xvalues, y=regression_line).data[0])
         
             # Return the Figure object to whatever calls this function
             return fig
    
+
     with ui.card():
         
+        @render.text
+        def method2():
+            return f"{function_options_dict[input.selected_second_method()]}"
+
         @render.text
         def descrip2():
-            return f"{function_options_dict[input.f1()]}"
-         
-        @render_plotly
-        def scatter_plot2() -> Figure:
-            ''' Define a function that returns a Plotly Figure'''
-        
-            # Fit linear regression model given our x and y lists
-            slope, intercept = statistics.linear_regression(xlist, ylist)
-        
-            # Create a new plotly scatter chart from our data
-            fig = px.scatter(
-                x=xlist2, 
-                y=ylist2, 
-                labels={'x': 'X', 'y': 'Y'}, 
-                title=f"Dataset 2 (Slope: {slope:.3f}, Intercept: {intercept:.3f})")
-        
-            # Calculate the regression line using list comprehension
-            # Return mx+b for each x in the x list
-            regression_line = [slope * x + intercept for x in xlist2]
-        
-            # Create a plotly line using px.line() constructor method.
-            # Use the figure's add_trace() method
-            # Pass in the new line we created to add it to the chart
-            fig.add_trace(px.line(x=xlist2, y=regression_line).data[0])
-        
-            # Return the Figure object to whatever calls this function
-            return fig
-  
-    with ui.card():
-        
-        @render.text
-        def descrip3():
-            return f"{function_options_dict[input.f2()]}"
+            return f"{description_mapping[input.selected_second_method()]}"
         
         @render_plotly
         def scatter_plot3() -> Figure:
             ''' Define a function that returns a Plotly Figure'''
         
-            # Fit linear regression model given our x and y lists
-            slope, intercept = statistics.linear_regression(xlist, ylist)
-        
-            # Create a new plotly scatter chart from our data
+            method = input.selected_second_method()
+            xvalues, yvalues = getXY()
+            slope, intercept = get_slope_and_intercept(xvalues, yvalues, method)
+            
             fig = px.scatter(
-                x=xlist, 
-                y=ylist, 
+                x=xvalues, 
+                y=yvalues, 
                 labels={'x': 'X', 'y': 'Y'}, 
-                title=f"Dataset 1 (Slope: {slope:.3f}, Intercept: {intercept:.3f})")
+                title=f"Slope: {slope:.3f}, Intercept: {intercept:.3f}")
         
-            # Calculate the regression line using list comprehension
-            # Return mx+b for each x in the x list
-            regression_line = [slope * x + intercept for x in xlist]
-        
-            # Create a plotly line using px.line() constructor method.
-            # Use the figure's add_trace() method
-            # Pass in the new line we created to add it to the chart
-            fig.add_trace(px.line(x=xlist, y=regression_line).data[0])
-        
-            # Return the Figure object to whatever calls this function
+            regression_line = [slope * x + intercept for x in xvalues]
+            fig.add_trace(px.line(x=xvalues, y=regression_line).data[0])
             return fig
    
-    with ui.card():
-        
-        @render.text
-        def descrip4():
-            return f"{function_options_dict[input.f2()]}"
-         
-        @render_plotly
-        def scatter_plot4() -> Figure:
-            ''' Define a function that returns a Plotly Figure'''
-        
-            # Fit linear regression model given our x and y lists
-            slope, intercept = statistics.linear_regression(xlist, ylist)
-        
-            # Create a new plotly scatter chart from our data
-            fig = px.scatter(
-                x=xlist2, 
-                y=ylist2, 
-                labels={'x': 'X', 'y': 'Y'}, 
-                title=f"Dataset 2 (Slope: {slope:.3f}, Intercept: {intercept:.3f})")
-        
-            # Calculate the regression line using list comprehension
-            # Return mx+b for each x in the x list
-            regression_line = [slope * x + intercept for x in xlist2]
-        
-            # Create a plotly line using px.line() constructor method.
-            # Use the figure's add_trace() method
-            # Pass in the new line we created to add it to the chart
-            fig.add_trace(px.line(x=xlist2, y=regression_line).data[0])
-        
-            # Return the Figure object to whatever calls this function
-            return fig
-                       
